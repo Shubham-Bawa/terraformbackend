52fa031ad157e2257a98203152998b9e1b65563a
Shubham-Bawa <51166582+Shubham-Bawa@users.noreply.github.com>
2024-11-25 10:25:35 +0530

    Update resource_s3.tf

--- resource_s3.tf
+++ resource_s3.tf
@@ -27,7 +27,7 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
 
 
 resource "aws_dynamodb_table" "locks" {
-  name = "Neeha-05July-dblocks"
+  name = "Neeha-902903July-dblocks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "LockID"
 
